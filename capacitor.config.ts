62867e6215034f83c1b1a16374d5ba3be4ef7caa
Luiz <luizpfern14@gmail.com>
2026-04-19 19:15:57 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sports-court-booking-system',
+  webDir: 'www'
+};
+
+export default config;
